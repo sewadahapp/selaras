e1ccb613d8f3ab99e8626e93651def89f5fabb98
Wicaksana Pratama <wicaksanapratama@gmail.com>
2026-09-19 17:34:22 +0700

    fix(docs): offset sticky navigation rails

--- packages/docs/app/components/DocsSidebar.vue
+++ packages/docs/app/components/DocsSidebar.vue
@@ -3,7 +3,11 @@ const { data: navigation } = useDocsNavigation()
 </script>
 
 <template>
-  <SPageAside class="selaras-docs-sidebar" aria-label="Documentation navigation">
+  <SPageAside
+    class="selaras-docs-sidebar"
+    aria-label="Documentation navigation"
+    :ui="{ root: 'top-16 h-[calc(100vh-4rem)]' }"
+  >
     <SContentNavigation :navigation="navigation ?? []" />
   </SPageAside>
 </template>
--- packages/docs/app/components/DocsTableOfContents.vue
+++ packages/docs/app/components/DocsTableOfContents.vue
@@ -10,7 +10,12 @@ const docsConfig = computed(() => appConfig.selarasDocs ?? {})
 </script>
 
 <template>
-  <SPageAside v-if="links.length && docsConfig.toc?.enabled !== false" class="selaras-docs-toc" aria-label="Table of contents">
+  <SPageAside
+    v-if="links.length && docsConfig.toc?.enabled !== false"
+    class="selaras-docs-toc"
+    aria-label="Table of contents"
+    :ui="{ root: 'top-16 h-[calc(100vh-4rem)]' }"
+  >
     <SContentToc :links="links" :title="docsConfig.toc?.title" />
   </SPageAside>
 </template>
