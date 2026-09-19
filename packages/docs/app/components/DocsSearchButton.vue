db431e943eb9b4b5463442173dfca2e97d7e7a94
Wicaksana Pratama <wicaksanapratama@gmail.com>
2026-09-19 22:56:43 +0700

    style(docs): soften search trigger

--- packages/docs/app/components/DocsSearchButton.vue
+++ packages/docs/app/components/DocsSearchButton.vue
@@ -5,7 +5,7 @@ const messages = useMessages()
 </script>
 
 <template>
-  <SButton variant="outline" color="neutral" size="sm" :icon="icons.search" aria-label="Search documentation" @click="open">
+  <SButton variant="soft" color="neutral" size="sm" :icon="icons.search" aria-label="Search documentation" @click="open">
     <span class="hidden sm:inline">{{ messages.search }}</span>
     <SKbd value="meta" size="sm" />
     <SKbd value="K" size="sm" />
